9d00e8828022ee4c3b0b04642f6c15eeea6f55dd
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-13 11:57:30 -0500

    update

--- examen/src/Examen1/CalculadoraPrincipal.java
+++ examen/src/Examen1/CalculadoraPrincipal.java
@@ -9,6 +9,17 @@ public class CalculadoraPrincipal {
 
         final Scanner ingresarNumero = null;
 
+        System.out.println("Ingrese un número");
+        ingresarNumero = new Scanner(System.in);
+        numero1 = ingresarNumero.nextInt();
+
+        System.out.println("Escriba la operación que desea hacer");
+        System.out.println("Suma");
+        System.out.println("Resta");
+        System.out.println("Multiplicacion");
+        System.out.println("Division");
+        ingresarNumero = new Scanner(System.in);
+
     }
 
 }
--- examen/src/Examen1/ConstructorOperaciones.java
+++ examen/src/Examen1/ConstructorOperaciones.java
@@ -2,14 +2,20 @@ package Examen1;
 
 public class ConstructorOperaciones extends CalculadoraPrincipal {
 
-        private final int numero1 = 0;
-        private final int numero2 = 0;
-        private final int resultado = 0;
+    private final int numero1 = 0;
+    private final int numero2 = 0;
+    private final int resultado = 0;
+    
+    void OperacionesAritmeticas() {
+    }
 
-SUMA(numero1 + numero2);
-
-
-
+            SUMA (this.numero1 + this.numero2);
+            RESTA(this.numero1 - this.numero2);
+            MULTIPLICACION (this.numero1 * this.numero2);
+            DIVISION (this.numero1 / this.numero2);
+                if
+        }
 
     }
-}
+
+
