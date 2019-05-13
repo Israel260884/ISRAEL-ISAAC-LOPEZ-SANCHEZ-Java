520649a7f4127eda9b01f8b928e790c02e62113b
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-13 10:23:11 -0500

    examen

---
+++ examen/src/examen/AlumnoLicenciatura.java
@@ -0,0 +1,18 @@
+package examen;
+
+public class AlumnoLicenciatura extends Alumno {
+    private String carrera;
+
+    public AlumnoLicenciatura() {
+    }
+
+//**getter carrera
+    public String getCarrera() {
+        return this.carrera;
+    }
+
+//**setter carrera
+    public void setCarrera() {
+        this.carrera = this.carrera;
+    }
+}
---
+++ examen/src/examen/AlumnoMaestria.java
@@ -0,0 +1,19 @@
+package examen;
+
+public class AlumnoMaestria extends Alumno {
+
+    private String maestria;
+
+    public AlumnoMaestria() {
+    }
+
+//**getter maestria
+    public String getMaestria() {
+        return this.maestria;
+    }
+
+//**setter maestria
+    public void setMaestria() {
+        this.maestria = this.maestria;
+    }
+}
