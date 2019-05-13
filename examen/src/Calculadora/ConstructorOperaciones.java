0318bff84b84d80944a4695e7eca8160aee2b2ee
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-13 11:40:16 -0500

    update

--- examen/src/Calculadora/ConstructorOperaciones.java
+++ examen/src/Calculadora/ConstructorOperaciones.java
@@ -1,10 +1,13 @@
 package Calculadora;
 
-public class ConstructorOperaciones {
+public class ConstructorOperaciones extends CalculadoraPrincipal {
 
-        private final int numero1 = null;
-        private final int numero2 = null;
-        private final int resultado = null;
+        private final Integer numero1 = null;
+        private final Integer numero2 = null;
+        private final Integer resultado = null;
+
+SUMA(numero1 + numero2);
+
 
 
 
