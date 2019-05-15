f15a64da69cf1727a17d8d6b6a5e5f2ebae12971
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-15 11:03:47 -0500

    update

--- Ejercicios/src/CadenasEjemplos.java
+++ Ejercicios/src/CadenasEjemplos.java
@@ -1,9 +1,11 @@
 
 public class CadenasEjemplos {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // TODO Auto-generated method stub
 
+        final String nombre = "ISRAEL ISAAC LOPEZ SANCHEZ";
+        final char[] arregloNombre = nombre.toCharArray();
     }
 
 }
