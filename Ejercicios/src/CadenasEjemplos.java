7ab31488544081110a7947c2ccb7052a6056a9c1
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-15 11:43:55 -0500

    update

--- Ejercicios/src/CadenasEjemplos.java
+++ Ejercicios/src/CadenasEjemplos.java
@@ -5,7 +5,18 @@ public class CadenasEjemplos {
         // TODO Auto-generated method stub
 
         final String nombre = "ISRAEL ISAAC LOPEZ SANCHEZ";
-        final char[] arregloNombre = nombre.toCharArray();
+
+        final char[] arregloNombre = nombre.toLowerCase().toCharArray();
+
+        int valorNombre = 0;
+        for (final char caracter : arregloNombre) {
+            System.out.println(Integer.valueOf(caracter) + ":" + caracter);
+
+            valorNombre = valorNombre + caracter;
+        }
+
+        System.out.println("El valor del nombre" + nombre.toLowerCase() + "es de:" + valorNombre);
+
     }
 
 }
