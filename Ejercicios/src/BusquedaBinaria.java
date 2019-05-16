f290467e4e6547e557852adfb706dee2a8e396fd
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-16 10:17:08 -0500

    update

--- Ejercicios/src/BusquedaBinaria.java
+++ Ejercicios/src/BusquedaBinaria.java
@@ -7,14 +7,23 @@ public class BusquedaBinaria {
         final int[] arregloBinario = { 34, 67, 89, 100, 290, 500, 720 };
 
         final int valorBuscado = 89;
-
         final int valorBajo = 0;
         final int valorAlto = arregloBinario.length;
 
         final int valorCentral = valorBajo + (valorAlto / 2);
 
         while ((valorBajo < valorAlto) && (arregloBinario[valorCentral] != valorBuscado)) {
+            if (valorBuscado < arregloBinario[valorCentral]) {
+                valorAlto = valorCentral - 1;
 
+            } else {
+                valorBajo = valorCentral + 1;
+                valorCentral = valorBajo / 2;
+            }
+
+        }
+        if (valorBuscado = arregloBinario[valorCentral]) {
+            ;
         }
     }
 
