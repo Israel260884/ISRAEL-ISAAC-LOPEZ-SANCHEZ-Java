6c0c33397fd50c5788862b94f108ce150d1bca00
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-16 09:57:19 -0500

    UPDATE

--- Ejercicios/src/BusquedaSecuencial.java
+++ Ejercicios/src/BusquedaSecuencial.java
@@ -6,21 +6,12 @@ public class BusquedaSecuencial {
 
         final String[] arregloBusqueda = new String[10];
 
+        arregloBusqueda = {A,B,C,D,E,F,G,H,I,J}
+
         final String palabra = null;
 
-        arregloBusqueda[0] = A;
-        arregloBusqueda[1] = B;
-        arregloBusqueda[2] = C;
-        arregloBusqueda[3] = D;
-        arregloBusqueda[4] = E;
-        arregloBusqueda[5] = F;
-        arregloBusqueda[6] = G;
-        arregloBusqueda[7] = H;
-        arregloBusqueda[8] = I;
-        arregloBusqueda[9] = J;
 
-        System.out.println("Escribe la palabra para búsqueda");
-        final Scanner busquedaPalabra
+
 
 
 
