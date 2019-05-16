8ff7cc1f8f7c1ab25bfc2c0005387444533193cb
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-16 09:38:21 -0500

    update

--- Ejercicios/src/BusquedaSecuencial.java
+++ Ejercicios/src/BusquedaSecuencial.java
@@ -1,9 +1,20 @@
 
 public class BusquedaSecuencial {
 
-    public static void main(String[] args) {
-     
-        
+    public static void main(final String[] args) {
+
+        final String[] arregloBusqueda = new String[10];
+
+        arregloBusqueda[0] = A;
+        arregloBusqueda[1] = B;
+        arregloBusqueda[2] = C;
+        arregloBusqueda[3] = D;
+        arregloBusqueda[4] = E;
+        arregloBusqueda[5] = F;
+        arregloBusqueda[6] = G;
+        arregloBusqueda[7] = H;
+        arregloBusqueda[8] = I;
+        arregloBusqueda[9] = J;
 
     }
 
