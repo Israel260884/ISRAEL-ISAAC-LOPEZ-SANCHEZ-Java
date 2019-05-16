aa294b9111830aa572925bfa7d3725b5c352bb32
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-16 12:14:24 -0500

    update

--- Ejercicios/src/ArregloBurbuja.java
+++ Ejercicios/src/ArregloBurbuja.java
@@ -2,6 +2,8 @@
 public class ArregloBurbuja {
 
     public static void main(final String[] args) {
+        final int arregloNumero[] = { 9, 5, 8, 2, 6, 1, 7, 3, 4 };
+        final int arregloBurbuja[];
 
     }
 
