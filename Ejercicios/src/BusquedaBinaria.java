f16acf988470fb2c9c4926bd95c1bc88ab6d6c9c
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-16 10:05:14 -0500

    update

--- Ejercicios/src/BusquedaBinaria.java
+++ Ejercicios/src/BusquedaBinaria.java
@@ -1,8 +1,17 @@
 
 public class BusquedaBinaria {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // TODO Auto-generated method stub
+
+        final int[] arregloBinario = { 34, 67, 89, 100, 290, 500, 720 };
+
+        final int valorBuscado = 89;
+
+        final int valorBajo = 0;
+        final int valorAlto = arregloBinario.length;
+
+        final int valorCentral = valorBajo + (ValorAlto / 2);
 
     }
 
