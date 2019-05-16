8b9c19f93232b99befb66d0cffe06a7292593834
Israel260884 <israelisaaclopezsanchez@hotmail.com>
2019-05-16 12:49:10 -0500

    update

--- Ejercicios/src/ArregloBurbuja.java
+++ Ejercicios/src/ArregloBurbuja.java
@@ -6,7 +6,11 @@ public class ArregloBurbuja {
         final int arregloBurbuja[];
 
         int i = arregloNumero.length;
+        final int a = 0;
+        final int b = 0;
 
+        if (arregloNumero[a] - arregloBurbuja[b]) {
+        }
         for (i = 0; i > arregloNumero.length; i--) {
 
         }
